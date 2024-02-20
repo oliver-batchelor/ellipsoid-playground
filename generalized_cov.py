2d4d0d8e3d8b34c5ba708e5adaae42372b9cf588
Oliver Batchelor <oliver.batchelor@canterbury.ac.nz>
2024-02-20 16:16:20 +1300

    Better way to do common expressions

--- generalized_cov.py
+++ generalized_cov.py
@@ -1,6 +1,6 @@
 import sympy as sp
 from sympy import exp, log
-x, y, u, v, a, b, c, p, B = sp.symbols('x y u v a b c p B')
+x, y, u, v, a, b, c, B = sp.symbols('x y u v a b c B')
 
 dx = x - u
 dy = y - v
@@ -8,16 +8,26 @@ dy = y - v
 inner =  (0.5 * (dx**2 * a + dy**2 * c) - dx * dy * b) 
 p = exp( -(inner ** B) )
 
-print (sp.diff(p, u))
 
+cse, derivs = sp.cse([sp.diff(p, i) for i in [u, v, a, b, c, B]] + [p],  optimizations='basic')
+
+print(dict(cse))
+print(derivs)
+
+def rev_cse(expr):
+  for (s, sub_expr) in reversed(cse):
+    expr = expr.subs(s, sub_expr)
+  return expr
+
+expanded = [rev_cse(deriv) for deriv in derivs]
+
+print(sp.simplify(expanded[-1]) == sp.simplify(p))
 
 du = -B*(0.5*a*(2*u - 2*x) + b*dy) * (inner ** B)  * p / inner
 dv = -B*(0.5*c*(2*v - 2*y) + b*dx) * (inner ** B)  * p / inner
 
 print(sp.diff(p, u) == du)
 print(sp.diff(p, v) == dv)
-
-
 
 da = -0.5*B * dx**2 * (inner ** B) * p / inner
 db = B * dx * dy * (inner ** B) * p / inner
